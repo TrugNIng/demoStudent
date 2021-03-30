e204db7d10240e81113e48473e91702df1986160
huyphong2510 <huyphong2510@gmail.com>
2021-03-30 13:42:14 +0700

    dfdf

--- src/main/java/com/example/bkacad/model/Student.java
+++ src/main/java/com/example/bkacad/model/Student.java
@@ -1,17 +1,36 @@
 package com.example.bkacad.model;
 
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String name;
+    @Column(name = "full_name")
+    private String full_name;
+
+    @Column(name = "faculty_id")
+    private String faculty_id;
+
+    @Column(name = "birthday")
+    private String birthday;
+
+    @Column(name = "address")
+    private String address;
+
+    public Long id() {
+        return id;
+    }
+
+    public void d(Long id) {
+        this.id = id;
+    }
 
     public Long getId() {
         return id;
@@ -21,11 +40,35 @@ public class Student {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFull_name() {
+        return full_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getFaculty_id() {
+        return faculty_id;
+    }
+
+    public void setFaculty_id(String faculty_id) {
+        this.faculty_id = faculty_id;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
