2710cff392d216e32c1ccc2207dea816933df633
huyphong2510 <huyphong2510@gmail.com>
2021-04-01 09:43:45 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/bkacad/controller/StudentController.java
    #	src/main/java/com/example/bkacad/model/Student.java
    #	src/main/java/com/example/bkacad/repository/StudentRepository.java
    #	src/main/java/com/example/bkacad/service/StudentService.java
    #	src/main/resources/application.properties

--- src/main/java/com/example/bkacad/model/Student.java
+++ src/main/java/com/example/bkacad/model/Student.java
@@ -21,7 +21,7 @@ public class Student {
     private  String birthday;
 
     @Column(name = "faculty_id")
-    private  String phone;
+    private  String faculty_id;
 
     @Column(name = "address")
     private  String address;
@@ -52,11 +52,11 @@ public class Student {
     }
 
     public String getPhone() {
-        return phone;
+        return faculty_id;
     }
 
     public void setPhone(String phone) {
-        this.phone = phone;
+        this.faculty_id = faculty_id;
     }
 
     public String getAddress() {
