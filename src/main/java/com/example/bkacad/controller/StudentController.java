2a6196fa0b8141c3e18a03eb81e61170c418e388
phongnh <huyphong2510@gmail.com>
2021-04-05 11:03:54 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/bkacad/controller/StudentController.java
    #	src/main/java/com/example/bkacad/model/Student.java
    #	src/main/java/com/example/bkacad/repository/StudentRepository.java
    #	src/main/java/com/example/bkacad/service/StudentService.java
    #	src/main/resources/application.properties

--- src/main/java/com/example/bkacad/controller/StudentController.java
+++ src/main/java/com/example/bkacad/controller/StudentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @RestController
@@ -45,7 +46,29 @@ public class StudentController {
         studentService.delete(student);
         return "Record deleted succesfully";
     }
+    @PostMapping("/bulkcreate")
+    public List<Student> createStudents(@RequestBody List<Student> students) {
+        List<Student> createResponse = studentService.saveAll(students);
+        return createResponse;
+    }
 
+    @PutMapping("/bulkupdate")
+    public List<Student> updateStudents(@RequestBody List<Student> students) {
+        List<Student> updateResponse = studentService.updateAll(students);
+        return updateResponse;
+    }
+
+    @GetMapping("/allstudent")
+    public List<Student> getStudents() {
+        List<Student> getresponse = studentService.getAll();
+        return getresponse;
+    }
+
+    @DeleteMapping("/bulkdelete")
+    public String deleteStudents(@RequestBody List<Student> students) {
+        studentService.deleteAll(students);
+        return "Records deleted succesfully";
+    }
 
 }
 
